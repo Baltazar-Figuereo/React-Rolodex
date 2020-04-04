9d1bb3742d9ad98f46ed08335c48a3c9411b5ca2
CodeSavant <bfiguereo20@gmail.com>
2020-04-04 19:32:23 -0400

    added grid for cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from "./components/card-list/card-list.component";
 
+class App extends Component
+{
+  constructor()
+  {
+    super();
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() 
+  {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        <CardList name='baltazar'>
+        {
+          this.state.monsters.map(monsters => <h1 key={monsters.id}>{monsters.name}</h1>)
+        }
+        </CardList>
+       
+      
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -10,7 +44,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. 
         </p>
-        <p>Baltazar Figuereo</p>
+        <p>Baltazar Figuereo Jr.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,5 +57,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
